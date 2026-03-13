b4df0beb5a89604e049de36367cfc8a7b6cfa6dc
vasilij_bf109g6 <vasilijparimatc@gmail.com>
2026-03-13 10:49:24 +0300

    a

---
+++ test_3_v2.py
@@ -0,0 +1,15 @@
+def calculate_total(prices, *discounts, **options):
+
+    total = sum(prices)
+
+    for discount in discounts:
+        total *= (1 - discount / 100)
+
+    tax = options.get("tax", 0)
+    total *= (1 + tax / 100)
+
+    round_to = options.get("round_to", 2)
+    if round_to is not None:
+        total = round(total, round_to)
+
+    return total
